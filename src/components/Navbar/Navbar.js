69394d8d7527db294f91dff953131869d1739787
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-08-15 00:31:24 -0400

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,45 @@
+import React from "react";
+import "./styles.css";
+import { Link, NavLink } from "react-router-dom";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
-import React from 'react';
-import './styles.css';
-import { Link, NavLink } from 'react-router-dom';
-import {Navbar, Container, Nav} from 'react-bootstrap';
-
+// Navbar component
 const AppNavbar = () => {
   return (
-    <Navbar expand="lg" data-bs-theme="light" sticky="top" className="navbar-style">
+    <Navbar
+      expand="lg"
+      data-bs-theme="light"
+      sticky="top"
+      className="navbar-style"
+    >
       <Container>
-        <Navbar.Brand as={Link} to="/" className='nav-brand'>🎬 MovieFinder</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" className="nav-brand">
+          <img
+            src="/assets/images/logo.svg"
+            alt="Movie Finder Logo"
+            className="logo"
+          />{" "}
+          MovieFinder
+        </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="main-nav" />
         <Navbar.Collapse id="main-nav">
           <Nav className="ms-auto nav-links">
-            <Nav.Link as={NavLink} end to="/">Home</Nav.Link>
-            <Nav.Link as={NavLink} to="/movies">Popular</Nav.Link>
-            <Nav.Link as={NavLink} to="/genres">Genres</Nav.Link>
-            <Nav.Link as={NavLink} to="/favorites">Favorites</Nav.Link>
-            <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
+            <Nav.Link as={NavLink} end to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/movies">
+              Movies
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/genres">
+              Genres
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/favorites">
+              Favorites
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/contact">
+              Contact
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>

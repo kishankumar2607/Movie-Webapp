92e57c41a89942b2582613909f982da4d7e2ddbb
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-08-15 00:30:59 -0400

    Update Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./styles.css";
 
+// Button Component
 const Button = ({ href, text, outline }) => {
   return !!outline ? (
     <Link to={href} className="btn btn-outline">
